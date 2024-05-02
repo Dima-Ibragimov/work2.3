39aa333211c92449f031a3b172f7f8adfdb3bd5a
Dima-Ibragimov <168379280+Dima-Ibragimov@users.noreply.github.com>
2024-05-02 22:18:15 +0300

    Add files via upload

---
+++ modul2.3.py
@@ -0,0 +1,7 @@
+bike = ['HONDA', 'BMW', 'HARLEY-DEVIDSON', 'INDIAN', 'BENELLI']
+for i in bike:
+    print('Я хочу купить ' + i)
+bike_count = 0
+for i in range(len(bike)):
+    bike_count += 10
+    print(bike_count)
